561566ead1aa2475b41233d9bb6d1f1bc3081748
linr <1101734174@qq.com>
2022-05-14 15:41:41 +0800

    更新

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,25 +5,26 @@ module.exports = {
   themeConfig: {
     activeHeaderLinks: false,
     nav: [
-      { text: "java", link: "/java/" },
-      { text: "关于我", link: "/about/" },
+      { text: "java", link: "/java/" }
     ],
     sidebar: [
       //左侧列表
       {
-        title: "Java",
+        title: "Java基础",
         path: '/java/',
         collapsable: false, // 可选的, 默认值是 true,
         sidebarDepth: 1,    // 可选的, 默认值是 1
         children: [
-          "/java/base/基础篇",
-          "/java/base/Java基础知识",
-          "/java/base/网络编程",
+          {title:"面向对象",
+          path:'/java/base/1.面向对象.md'},
+          {title:"封装、继承、多态",
+          collapsable: false, // 可选的, 默认值是 true,
+          children: [
+            '/java/base/polymorphism/2.封装、继承、多态',
+            '/java/base/polymorphism/3.方法重写与重载.md',
+            '/java/base/polymorphism/4.Java的继承与实现.md'
+          ]}
         ]
-      },
-      {
-        title: "关于我",
-        path: '/about/'
       }
     ],
   },
--- docs/README.md
+++ docs/README.md
@@ -6,10 +6,21 @@ actionText: 开始阅读 →
 actionLink: /java/
 features:
 - title: 关于我
-  details: 一个普普通通的码农
-- title: 算法
-  details: 一天一道算法题，准备逆天。
+  details: 一个普普通通的码农，想在时代的巨浪的平稳度过一身
 - title: 生活点点滴滴
-  details: 记录生活中美好的时刻。
-footer: Hep Licensed | Copyright © 2021-present linrong
+  details: 记录生活，也希望自己在世界上存在一点痕迹
+footer: Hep Licensed | Copyright © 2021-present lr
 ---
+### 网站里面有什么
+
+即将会更新
+
+* Java基础
+* Java并发编程
+* MySQL/Oracle 数据库
+* JVM调优
+* 算法导论
+* 设计模式
+* 高并发/高可用
+* 分布式
+* 拓展篇章
